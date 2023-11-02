e58a4f1b1361df48e75152b0159ee2ae0e04a2b8
adlersuarez <adlersuarez2904@gmail.com>
2023-11-02 17:29:19 -0500

    Solucionado Aside

--- src/view/pages/layout/aside/Aside.tsx
+++ src/view/pages/layout/aside/Aside.tsx
@@ -15,120 +15,143 @@ type Props = {
     onEventOverlay: React.MouseEventHandler,
 }
 
-type MenuItem = {
+/*type MenuItem = {
     id: string,
     nombre: string,
     pathname?: string,
     icon: string,
     subMenus?: MenuItem[]
-}
+}*/
 
-/*type MenuItem = {
-    id: number,
+type MenuItem = {
+    id: string,
     titulo: string,
     url?: string,
     icono: string,
-    moduPadre: number,
+    moduPadre: boolean,
     modPosicion: number,
     subMenu: boolean,
     subMenuItems?: MenuItem[]
-}*/
+}
 
 const menus: MenuItem[] = [
     {
         id: "1",
-        nombre: "Inicio",
-        pathname: "/inicio/inscripcion",
-        icon: "bi-info-circle",
-        subMenus: []
+        titulo: "Inicio",
+        url: "/inicio/inscripcion",
+        icono: "bi-info-circle",
+        moduPadre: false,
+        modPosicion: 1,
+        subMenu: false,
+        subMenuItems: []
     },
     {
         id: "2",
-        nombre: "Formatos",
-        icon: "bi-file-earmark-text",
-        pathname: "/inicio/formatos",
-        subMenus: []
+        titulo: "Formatos",
+        url: "/inicio/formatos",
+        icono: "bi-file-earmark-text",
+        moduPadre: false,
+        modPosicion: 2,
+        subMenu: false,
+        subMenuItems: []
     },
     {
         id: "3",
-        nombre: "Proceso",
-        pathname: "/inicio/proceso",
-        icon: "bi-bar-chart-steps",
-        subMenus: []
+        titulo: "Proceso",
+        url: "/inicio/proceso",
+        icono: "bi-bar-chart-steps",
+        moduPadre: false,
+        modPosicion: 3,
+        subMenu: false,
+        subMenuItems: []
     },
     {
         id: "4",
-        nombre: "Reglamentos",
-        pathname: "/inicio/reglamentos",
-        icon: "bi-file-ruled",
-        subMenus: []
+        titulo: "Reglamentos",
+        url: "/inicio/reglamentos",
+        icono: "bi-file-ruled",
+        moduPadre: false,
+        modPosicion: 4,
+        subMenu: false,
+        subMenuItems: []
     },
     {
         id: "5",
-        nombre: "Contactos",
-        pathname: "/inicio/contactos",
-        icon: "bi-envelope-at",
-        subMenus: []
+        titulo: "Contactos",
+        url: "/inicio/contactos",
+        icono: "bi-envelope-at",
+        moduPadre: false,
+        modPosicion: 5,
+        subMenu: false,
+        subMenuItems: []
     },
     {
         id: "6",
-        nombre: "Docente",
-        pathname: "/inicio/revision",
-        icon: "bi-file-ruled",
-        subMenus: []
+        titulo: "Docente",
+        url: "/inicio/revision",
+        icono: "bi-file-ruled",
+        moduPadre: false,
+        modPosicion: 6,
+        subMenu: false,
+        subMenuItems: []
     },
     {
         id: "7",
-        nombre: "Administrador",
-        pathname: "/inicio/administrador",
-        icon: "bi-bar-chart-line-fill",
-        subMenus: []
+        titulo: "Administrador",
+        url: "/inicio/administrador",
+        icono: "bi-bar-chart-line-fill",
+        moduPadre: false,
+        modPosicion: 7,
+        subMenu: false,
+        subMenuItems: []
     },
-
-    /*
-    {
-        id: "8",
+    /*{
+        id: '8',
         titulo: "Dashboard",
         url: "/inicio/dashboard",
         icono: "bi-speedometer",
-        moduPadre: 0,
+        moduPadre: false,
         modPosicion: 1,
         subMenu: false,
         subMenuItems: [],
     },
     {
-        id: "9",
+        id: '9',
         titulo: "Marcación",
         url: "",
         icono: "bi-arrow-left-right",
-        moduPadre: 0,
+        moduPadre: true,
         modPosicion: 2,
         subMenu: true,
         subMenuItems: [
             {
-                id: 7,
+                id: '7',
                 titulo: "Visitantes",
                 url: "/inicio/marcacion-visitante",
                 icono: "bi-dash",
-                moduPadre: 2,
-                modPosicion: 1
+                moduPadre: false,
+                modPosicion: 1,
+                subMenu: false,
             }
             ,
             {
-                id: 8,
+                id: '8',
                 titulo: "Estudiantes",
                 url: "/inicio/marcacion-estudiante",
                 icono: "bi-dash",
-                moduPadre: 2,
-                modPosicion: 2
+                moduPadre: false,
+                modPosicion: 2,
+                subMenu: false,
+
             },
             {
-                id: 9,
+                id: '9',
                 titulo: "Personal",
                 url: "/inicio/marcacion-personal",
                 icono: "bi-dash",
-                moduPadre: 2,
-                modPosicion: 3
+                moduPadre: false,
+                modPosicion: 3,
+                subMenu: false,
             }
 
         ],
@@ -149,30 +172,30 @@ const Aside = (props: Props) => {
                 <ul id="menus">
                     {
                         menus.map((menu, index) => {
-                            if (menu.subMenus?.length == 0) {
+                            if (menu.subMenuItems?.length == 0) {
                                 return <Menu
                                     key={index}
                                     pathname={props.pathname}
-                                    icon={menu.icon}
-                                    nombre={menu.nombre}
-                                    to={menu.pathname!}
+                                    icon={menu.icono}
+                                    nombre={menu.titulo}
+                                    to={menu.url!}
                                 />
                             } else {
                                 return <ListMenu
                                     key={index}
                                     idList={menu.id}
-                                    desplegar={menu.subMenus?.filter(item => item.pathname === props.pathname).length != 0}
-                                    icon={menu.icon}
-                                    nombre={menu.nombre}
+                                    desplegar={menu.subMenuItems?.filter(item => item.url === props.pathname).length != 0}
+                                    icon={menu.icono}
+                                    nombre={menu.titulo}
                                 >
                                     {
-                                        menu.subMenus?.map((submenu, indexm) => {
+                                        menu.subMenuItems?.map((submenu, indexm) => {
                                             return <Menu
                                                 key={indexm}
                                                 pathname={props.pathname}
-                                                icon={submenu.icon}
-                                                nombre={submenu.nombre}
-                                                to={submenu.pathname!}
+                                                icon={submenu.icono}
+                                                nombre={submenu.titulo}
+                                                to={submenu.url!}
                                             />
                                         })
                                     }
