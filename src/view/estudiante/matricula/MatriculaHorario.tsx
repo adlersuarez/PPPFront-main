92933f1e91be0ec93fc75f55c3d36007d203ba6e
R4mss <67804499+R4mss@users.noreply.github.com>
2023-11-07 12:58:12 -0500

    horario

--- src/view/estudiante/matricula/MatriculaHorario.tsx
+++ src/view/estudiante/matricula/MatriculaHorario.tsx
@@ -12,96 +12,96 @@ const MatriculaHorario = (props: RouteComponentProps<{}>) => {
                     <table className="w-full border-collapse border">
                         <thead>
                             <tr className="font-bold text-lg">
-                                <th className="border p-2">
+                                <th className="border">
                                     Hora
                                 </th>
-                                <th className="border p-2">
+                                <th className="border">
                                     Lunes
                                 </th>
-                                <th className="border p-2">
+                                <th className="border">
                                     Martes
                                 </th>
-                                <th className="border p-2">
+                                <th className="border">
                                     Miercoles
                                 </th>
-                                <th className="border p-2">
+                                <th className="border">
                                     Jueves
                                 </th>
-                                <th className="border p-2">
+                                <th className="border">
                                     Viernes
                                 </th>
-                                <th className="border p-2">
+                                <th className="border">
                                     Sabado
                                 </th>
-                                <th className="border p-2">
+                                <th className="border">
                                     Domingo
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr className="h-24">
-                                <td className="border p-2">
+                                <td className="border">
                                     08:00
                                 </td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
                             </tr>
                             <tr className="h-24">
-                                <td className="border p-2">
+                                <td className="border">
                                     09:30
                                 </td>
-                                <td className="border p-2">
-                                    <div className="bg-green-300 w-full">
+                                <td className="border">
+                                    <div className="w-full h-full p-0 m-0 row-span-2 bg-blue-300">
                                         abcs
                                     </div>
                                 </td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
                             </tr>
                             <tr className="h-24">
-                                <td className="border p-2">
+                                <td className="border">
                                     11:00
                                 </td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
                             </tr>
                             <tr className="h-24">
-                                <td className="border p-2">
+                                <td className="border">
                                     12:30
                                 </td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
                             </tr>
                             <tr className="h-24">
-                                <td className="border p-2">
+                                <td className="border">
                                     02:00
                                 </td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
-                                <td className="border p-2"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
+                                <td className="border"></td>
                             </tr>
                         </tbody>
                     </table>
