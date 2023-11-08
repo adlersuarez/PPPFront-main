e817b540d8c5241797a62e9dcdc7862adcae82d1
R4mss <67804499+R4mss@users.noreply.github.com>
2023-11-08 12:18:43 -0500

    nuevo acceso interno y externo

--- src/view/acceso/Acceso.tsx
+++ src/view/acceso/Acceso.tsx
@@ -9,13 +9,15 @@ import { login } from "../../store/authSlice.store";
 import Button from "./widget/Button";
 import InputPassword from "./widget/InputPassword";
 import InputClave from "./widget/InputClave";
-import Checked from "./widget/Checked";
 import { LoginRest } from "../../network/rest/services.network";
 import Response from "../../model/class/response.model.class";
 import RestError from "../../model/class/resterror.model.class";
 import Login from "../../model/interfaces/login.model.interface";
 import { AiFillWarning } from "react-icons/ai";
 import { Types } from "../../model/enum/types.model";
+
+import FormRegistroEstudianteExterno from "./FormRegistroEstudianteExterno";
+//import Checked from "./widget/Checked";
 
 const Acceso = (props: RouteComponentProps<{}>) => {
 
@@ -31,6 +33,8 @@ const Acceso = (props: RouteComponentProps<{}>) => {
 
     const [codigoMensaje, setCodigoMensaje] = useState<string>('');
     const [claveMensaje, setClaveMensaje] = useState<string>('');
+
+    const [formRegEstExterno, setFormRegEstExterno] = useState<boolean>(false);
 
     const refCodigo = useRef<HTMLInputElement>(null);
     const refClave = useRef<HTMLInputElement>(null);
@@ -94,82 +98,105 @@ const Acceso = (props: RouteComponentProps<{}>) => {
         return <Redirect to="/inicio" />
     }
 
-    return <>
-        <div className="flex flex-wrap w-screen h-screen">
-            <div className="bg-portada relative -z-20 bg-cover hidden md:flex md:w-1/2 xl:w-2/3">
-                <div className="bg-sombra w-full  h-screen absolute -z-10"></div>
-                <div className="m-auto text-white text-center">
-                    <motion.p
-                        className="text-lg md:text-xl lg:text-3xl font-mont font-thin"
-                        initial={{ x: -1000, opacity: 0 }}
-                        transition={{ duration: 0.8 }}
-                        animate={{ x: 0, opacity: 1 }}
-                        exit={{ opacity: 0 }}>Bienvenidos al Sistema</motion.p>
-                    <motion.h1 className="text-2xl md:text-4xl lg:text-6xl font-mont font-bold"
-                        initial={{ x: -1000, opacity: 0 }}
-                        animate={{ x: 0, opacity: 1 }}
-                        transition={{ duration: 1 }}
-                    >CENTRO DE IDIOMAS</motion.h1>
-                    <motion.p
-                        className="text-lg md:text-xl lg:text-2xl font-mont font-thin flex justify-center items-center"
-                        initial={{ x: -1000, opacity: 0 }}
-                        transition={{ duration: 0.8 }}
-                        animate={{ x: 0, opacity: 1 }}
-                        exit={{ opacity: 0 }}> <span className="px-1">UPLA</span> <AcedemicCapSvg size={"w-8 h-8"} /></motion.p>
-                </div>
-            </div>
-            <div className="px-6 md:px-12 sm:px-10 my-auto w-full md:w-1/2 xl:w-1/3">
-                <div className="shadow-lg border">
-                    <motion.img
-                        className="m-auto w-32 pt-4"
-                        animate={{
-                            scale: [1, 1.5, 1.5, 1, 1],
-                            rotate: [0, 0, 270, 270, 0],
-                            borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-                        }}
-                        transition={{ duration: 2 }}
-                        src={images.logo}
-                        alt=""
-                    />
-                    <div className="flex flex-col items-center my-5">
-                        <p className="font-mont text-center my-1">Por favor ingrese a su cuenta</p>
-                        <p className="md:hidden text-center font-mont">INTRANET</p>
-                        <p className="md:hidden text-center font-mont text-sm flex">
-                            <span className="px-1">(INT)</span> <AcedemicCapSvg />
-                        </p>
-                        {mensaje != "" && <p className="text-red-600 flex items-center"><AiFillWarning className="mr-1" /> <span>{mensaje}</span></p>}
+    const onEventFormRegEstExterno = () => {
+        setFormRegEstExterno(!formRegEstExterno)
+    }
 
-                        <form className="w-full lg:px-12 md:px-8 px-4" onSubmit={onEventAcceso}>
-                            <InputClave
-                                refCodigo={refCodigo}
-                                codigo={codigo}
-                                codigoMensaje={codigoMensaje}
-                                setCodigoMessage={setCodigoMensaje}
-                                setCodigo={setCodigo}
-                                setMensaje={setMensaje}
-                            />
+    return (
+        <>
+            {
+                formRegEstExterno == false ?
+                    (
+                        <div className="flex flex-wrap w-screen h-screen">
+                            <div className="bg-portada relative -z-20 bg-cover hidden md:flex md:w-1/2 xl:w-2/3">
+                                <div className="bg-sombra w-full  h-screen absolute -z-10"></div>
+                                <div className="m-auto text-white text-center">
+                                    <motion.p
+                                        className="text-lg md:text-xl lg:text-3xl font-mont font-thin"
+                                        initial={{ x: -1000, opacity: 0 }}
+                                        transition={{ duration: 0.8 }}
+                                        animate={{ x: 0, opacity: 1 }}
+                                        exit={{ opacity: 0 }}>Bienvenidos al Sistema</motion.p>
+                                    <motion.h1 className="text-2xl md:text-4xl lg:text-6xl font-mont font-bold"
+                                        initial={{ x: -1000, opacity: 0 }}
+                                        animate={{ x: 0, opacity: 1 }}
+                                        transition={{ duration: 1 }}
+                                    >CENTRO DE IDIOMAS</motion.h1>
+                                    <motion.p
+                                        className="text-lg md:text-xl lg:text-2xl font-mont font-thin flex justify-center items-center"
+                                        initial={{ x: -1000, opacity: 0 }}
+                                        transition={{ duration: 0.8 }}
+                                        animate={{ x: 0, opacity: 1 }}
+                                        exit={{ opacity: 0 }}> <span className="px-1">UPLA</span> <AcedemicCapSvg size={"w-8 h-8"} /></motion.p>
+                                </div>
+                            </div>
+                            <div className="px-6 md:px-12 sm:px-10 my-auto w-full md:w-1/2 xl:w-1/3">
+                                <div className="shadow-lg border">
+                                    <motion.img
+                                        className="m-auto w-32 pt-4"
+                                        animate={{
+                                            scale: [1, 1.5, 1.5, 1, 1],
+                                            rotate: [0, 0, 270, 270, 0],
+                                            borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+                                        }}
+                                        transition={{ duration: 2 }}
+                                        src={images.logo}
+                                        alt=""
+                                    />
+                                    <div className="flex flex-col items-center my-5">
+                                        <p className="font-mont text-center my-1">Por favor ingrese a su cuenta</p>
+                                        <p className="md:hidden text-center font-mont">CENTRO DE IDIOMAS</p>
+                                        <p className="md:hidden text-center font-mont text-sm flex">
+                                            <span className="px-1">UPLA</span> <AcedemicCapSvg />
+                                        </p>
+                                        {mensaje != "" && <p className="text-red-600 flex items-center"><AiFillWarning className="mr-1" /> <span>{mensaje}</span></p>}
 
-                            <InputPassword
-                                see={ver}
-                                refClave={refClave}
-                                clave={clave}
-                                setClaveMensaje={setClaveMensaje}
-                                setClave={setClave}
-                                onEvenVerClave={onEvenVerClave}
-                                claveMensaje={claveMensaje}
-                                setMensaje={setMensaje}
-                            />
+                                        <form className="w-full lg:px-12 md:px-8 px-4" onSubmit={onEventAcceso}>
+                                            <InputClave
+                                                refCodigo={refCodigo}
+                                                codigo={codigo}
+                                                codigoMensaje={codigoMensaje}
+                                                setCodigoMessage={setCodigoMensaje}
+                                                setCodigo={setCodigo}
+                                                setMensaje={setMensaje}
+                                            />
 
-                            <Checked />
+                                            <InputPassword
+                                                see={ver}
+                                                refClave={refClave}
+                                                clave={clave}
+                                                setClaveMensaje={setClaveMensaje}
+                                                setClave={setClave}
+                                                onEvenVerClave={onEvenVerClave}
+                                                claveMensaje={claveMensaje}
+                                                setMensaje={setMensaje}
+                                            />
 
-                            <Button proceso={proceso} />
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-    </>
+                                            {/* <Checked /> */}
+                                            <br />
+
+                                            <Button proceso={proceso} />
+
+                                            <br />
+
+                                            <span className="text-xs cursor-pointer text-upla-100 hover:underline"
+                                                onClick={onEventFormRegEstExterno}>Registro estudiante externo</span>
+
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                    :
+                    (
+                        <FormRegistroEstudianteExterno onEventFormRegEstExterno={onEventFormRegEstExterno}/>
+                    )
+            }
+
+
+        </>
+    )
 }
 
 export default Acceso;
