d365173da3f327afb1d4fa108945726101ae0e6a
adlersuarez <adlersuarez2904@gmail.com>
2023-11-15 10:49:33 -0500

    Responsive de notificaciones

--- src/view/pages/layout/nav/Nav.tsx
+++ src/view/pages/layout/nav/Nav.tsx
@@ -43,7 +43,7 @@ type Props = {
 
 const Nav = (props: Props) => {
 
-    
+
 
     const dispatch = useDispatch();
 
@@ -68,27 +68,26 @@ const Nav = (props: Props) => {
             <button
                 onClick={props.onEventMenu}
                 className="flex items-center justify-center px-4 text-white hover:bg-white hover:text-upla-100">
-                <i className="bi bi-justify text-2xl"/>
+                <i className="bi bi-justify text-2xl" />
             </button>
-            <ul className="flex items-center flex-1 justify-end pr-3 h-full">
+            <ul className="flex items-center flex-1 justify-end sm:pr-3 h-full">
                 <li className="flex justify-center h-full relative">
                     <button className="px-4 text-white hover:bg-white hover:text-upla-100 relative"
                         onClick={() => setOpen(!open)}>
-                        <i className="bi bi-bell text-xl"></i>
+                        <i className="bi bi-bell text-xl" />
                         <span className="absolute right-[5px] top-[2px] bg-yellow-300 text-yellow-800 text-xs font-medium px-1.5 py-0.5 rounded text-center">{notif.length}</span>
                     </button>
-                    <ul className={`${open ? "block" : "hidden"}  absolute h-fit translate-x-[-270px] translate-y-[53px] inset-0  w-80 bg-white rounded-sm drop-shadow-md`}>
+                    <ul className={`${open ? "block" : "hidden"} absolute h-fit translate-x-[-243px] translate-y-[53px] sm:translate-x-[-270px] inset-0 w-80 bg-white rounded-sm drop-shadow-md`}>
                         <div className="max-h-[220px] overflow-y-auto">
                             {
-
                                 notif.map((item, index) => {
                                     return (
                                         <li key={index}>
                                             <a className="flex flex-row items-center justify-between px-2 py-5 border-b-2 border-[#ddd] border-solid m-0">
                                                 <div className="p-3">
                                                     <div className="flex items-center justify-center relative w-12">
-                                                        <div className="absolute left-0"><i className="bi bi-circle-fill text-5xl text-upla-100"></i> </div>
-                                                        <div className="absolute left-3"><i className="bi bi-chat-dots-fill text-2xl text-white"></i></div>
+                                                        <div className="absolute left-0"><i className="bi bi-circle-fill text-5xl text-upla-100" /> </div>
+                                                        <div className="absolute left-3"><i className="bi bi-chat-dots-fill text-2xl text-white" /></div>
                                                     </div>
                                                 </div>
                                                 <div className="ml-1 flex-1 w-full">
@@ -104,7 +103,7 @@ const Nav = (props: Props) => {
                                                         onClick={() => removeNotif(item.id)}
                                                         className="focus:outline-none text-black bg-white hover:bg-red-800 hover:text-white focus:ring-4 focus:ring-red-300  rounded-md text-sm px-3 py-2"
                                                     >
-                                                        <i className="bi bi-x-lg"></i>
+                                                        <i className="bi bi-x-lg" />
                                                     </button>
                                                 </div>
                                             </a>
@@ -127,7 +126,7 @@ const Nav = (props: Props) => {
                     <button
                         onClick={() => dispatch(logout())}
                         className="px-4 text-white hover:bg-white hover:text-upla-100">
-                        <i className="bi bi-person-circle text-xl"></i>
+                        <i className="bi bi-person-circle text-xl" />
                     </button>
                 </li>
             </ul>
