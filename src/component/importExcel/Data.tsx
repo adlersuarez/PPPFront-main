83267dc972d515a5921daf7fb90e0c97b4405d7a
R4mss <67804499+R4mss@users.noreply.github.com>
2023-12-01 08:40:19 -0500

    act

--- src/component/importExcel/Data.tsx
+++ src/component/importExcel/Data.tsx
@@ -1,8 +1,49 @@
-const Data = () => {
-    return(
-        <>
-        
-        </>
-    )
+import React, { useState } from 'react';
+import * as XLSX from 'xlsx';
+
+interface ExcelDataItem {
+    [key: string]: any;
 }
+
+type ExcelData = ExcelDataItem[];
+
+const Data = () => {
+
+    const [excelData, setExcelData] = useState<ExcelData | null>(null);
+
+    const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                const data = new Uint8Array(event.target?.result as ArrayBuffer);
+                const workbook = XLSX.read(data, { type: 'array' });
+                const sheetName = workbook.SheetNames[0];
+                const sheet = workbook.Sheets[sheetName];
+                const jsonData = XLSX.utils.sheet_to_json<ExcelData>(sheet, { header: 1 });
+
+                setExcelData(jsonData);
+            };
+
+            reader.readAsArrayBuffer(file);
+        }
+    };
+
+    console.log(excelData)
+
+    return (
+        <div>
+            <input type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
+            {excelData && (
+                <div>
+                    <h2>Excel Data:</h2>
+                    <pre>{JSON.stringify(excelData, null, 2)}</pre>
+
+                </div>
+            )}
+        </div>
+    );
+};
+
 export default Data
--- src/view/docente/SubirNotas.tsx
+++ src/view/docente/SubirNotas.tsx
@@ -1,4 +1,5 @@
 import CustomModal from "@/component/Modal.component";
+import Data from "@/component/importExcel/Data";
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
@@ -101,8 +102,14 @@ const SubirNotas = () => {
                     </div>
                 </div>
             </div> */}
-            <div>
-                
+            <div className="flex flex-wrap -mx-3">
+                <div className="w-full max-w-full px-3 flex-0">
+                    <div className="flex flex-col visible w-full h-auto min-w-0 p-4 break-words bg-white opacity-100 border rounded-md bg-clip-border">
+
+                        <Data/>
+
+                    </div>
+                </div>
             </div>
         </>
     )
