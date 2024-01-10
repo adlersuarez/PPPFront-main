ae6099f31a7b29ddab101274f8c74bf87e155900
OIS <ois@iMac-de-OIS.local>
2024-01-10 08:00:29 -0500

    cambio ar rango notas

--- src/view/trabajador/nota/component/notas/NotaDos.tsx
+++ src/view/trabajador/nota/component/notas/NotaDos.tsx
@@ -1,4 +1,3 @@
-
 import { keyNumberFloat } from "@/helper/herramienta.helper";
 import { isNumeric } from "@/helper/herramienta.helper";
 import { useRef, useState } from "react"
@@ -7,8 +6,8 @@ const NotaDos = () => {
 
     const [nota2, setNota2] = useState<string>("0")
 
-    const [valid, setValid] = useState<boolean>(false)
- 
+    const [valid, setValid] = useState<boolean>(true)
+
     const refNota2 = useRef<HTMLInputElement>(null)
 
     const selectAllText = () => {
@@ -21,46 +20,39 @@ const NotaDos = () => {
         event.preventDefault(); // Evitar la acción de pegado
     };
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {    
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const inputValue = event.target.value;
+
         setNota2(inputValue)
 
-        if(isNumeric(inputValue)){
-            if(parseFloat(inputValue)<=0 || parseFloat(inputValue)>20){
-                console.log("ee")
-                setValid(true)
-            }else{
-                setValid(false)
+        if (inputValue.trim() == '') {
+
+            setValid(false); // Si está vacío, se establece como inválido
+            return
+        } 
+        
+        if (isNumeric(inputValue)) {
+            if (parseFloat(inputValue) >= 0 && parseFloat(inputValue) <= 20) {
+                setValid(true); // Si es numérico y está dentro del rango, se establece como válido
+            } else {
+                setValid(false); // Si es numérico pero está fuera del rango, se establece como inválido
             }
-        }else{
-            setValid(false)
         }
     };
 
-    const handleNextInput = (event: React.KeyboardEvent<HTMLInputElement>) =>{
-        if (event.key === 'Enter') {
-            const nextInput = event.target.parentElement.parentElement.nextElementSibling.querySelector('input',);
-            nextInput.focus();
-          }
-        //   if (event.key === 'Enter' && isLastRow) {
-        //     const firstInput = event.target.parentElement.parentElement.parentElement.querySelector('input',);
-        //     firstInput.focus();
-        //   }
-    }
-    
     return (
         <>
             <input
                 type="text"
                 maxLength={5}
-                className={`font-mont border ${valid?"bg-red-300":""} border-gray-300 text-gray-900 rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-1 text-center`}
+                className={`font-mont border ${valid ? "border-gray-300" : "bg-red-300"} text-gray-900 rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-1 text-center`}
                 ref={refNota2}
                 value={nota2}
                 onChange={handleChange}
                 onClick={selectAllText}
                 onPaste={handlePaste}
-                onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => keyNumberFloat(event)} 
-                onKeyUp={(event: React.KeyboardEvent<HTMLInputElement>)=>handleNextInput(event)}
+                onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => keyNumberFloat(event)}
+                // onKeyUp={(event: React.KeyboardEvent<HTMLInputElement>) => handleNextInput(event)}
             />
         </>
     )
--- src/view/trabajador/nota/component/notas/NotaUno.tsx
+++ src/view/trabajador/nota/component/notas/NotaUno.tsx
@@ -1,10 +1,12 @@
-
-// import { keyNumberFloat, handleNumericInput } from "@/helper/herramienta.helper";
+import { keyNumberFloat } from "@/helper/herramienta.helper";
+import { isNumeric } from "@/helper/herramienta.helper";
 import { useRef, useState } from "react"
 
 const NotaUno = () => {
 
-    const [nota1, setNota1] = useState<number>(0)
+    const [nota1, setNota1] = useState<string>("0")
+
+    const [valid, setValid] = useState<boolean>(true)
 
     const refNota1 = useRef<HTMLInputElement>(null)
 
@@ -14,37 +16,54 @@ const NotaUno = () => {
         }
     };
 
+    const handlePaste = (event: React.ClipboardEvent<HTMLInputElement>) => {
+        event.preventDefault(); // Evitar la acción de pegado
+    };
+
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        let inputValue = parseFloat(event.target.value);
+        const inputValue = event.target.value;
 
-        if (inputValue > 20) {
-            inputValue = 20;
-        } else if (inputValue < 0) {
-            inputValue = 0;
-        }
+        setNota1(inputValue)
 
-        if (!isNaN(inputValue)) {
-            // Limitar a dos decimales y eliminar ceros a la derecha
-            const roundedValue = parseFloat(inputValue.toFixed(2)); // Limitar a dos decimales
-            const trimmedValue = parseFloat(roundedValue.toString()); // Convertir a cadena y luego a número para eliminar ceros a la derecha
-            setNota1(trimmedValue);
-        } else {
-            setNota1(0);
+        if (inputValue.trim() == '') {
+
+            setValid(false); // Si está vacío, se establece como inválido
+            return
+        } 
+        
+        if (isNumeric(inputValue)) {
+            if (parseFloat(inputValue) >= 0 && parseFloat(inputValue) <= 20) {
+                setValid(true); // Si es numérico y está dentro del rango, se establece como válido
+            } else {
+                setValid(false); // Si es numérico pero está fuera del rango, se establece como inválido
+            }
         }
     };
+
+    const handleNextInput = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        // if (event.key === 'Enter') {
+        //     const nextInput = event.target.parentElement.parentElement.nextElementSibling.querySelector('input',);
+        //     nextInput.focus();
+        // }
+        //   if (event.key === 'Enter' && isLastRow) {
+        //     const firstInput = event.target.parentElement.parentElement.parentElement.querySelector('input',);
+        //     firstInput.focus();
+        //   }
+    }
 
     return (
         <>
             <input
-                type="number"
-                step="0.01"
+                type="text"
                 maxLength={5}
-                className="font-mont border border-gray-300 text-gray-900 rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-1 text-center"
+                className={`font-mont border ${valid ? "border-gray-300" : "bg-red-300"} text-gray-900 rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-1 text-center`}
                 ref={refNota1}
                 value={nota1}
                 onChange={handleChange}
                 onClick={selectAllText}
-            // onKeyUp={(event: React.KeyboardEvent<HTMLInputElement>) => handleNumericInput(event)} 
+                onPaste={handlePaste}
+                onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => keyNumberFloat(event)}
+                // onKeyUp={(event: React.KeyboardEvent<HTMLInputElement>) => handleNextInput(event)}
             />
         </>
     )
